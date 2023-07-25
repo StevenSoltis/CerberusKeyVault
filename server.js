b57d33c842f19b776d329a0029f021306b0210a3
StevenSoltis <stevensoltis83@gmail.com>
2023-07-24 20:09:40 -0400

    MainBranch

---
+++ server.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+    loginForm.addEventListener('submit', handleLoginFormSubmit);
+});
+
+function handleLoginFormSubmit(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // For demonstration purposes, we're just logging the credentials in the console.
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    // Here you can add the code to validate the credentials and perform the login process.
+}
