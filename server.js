fb0f99d8f8db076faca5fd3740e5e9c44b63cb71
StevenSoltis <stevensoltis83@gmail.com>
2023-07-26 07:05:16 -0400

    Morning Update 7/26

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ function handleLoginFormSubmit(event) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // For demonstration purposes, we're just logging the credentials in the console.
+
     console.log('Username:', username);
     console.log('Password:', password);
 
-    // Here you can add the code to validate the credentials and perform the login process.
+  
 }
